0a46b0ef5a662044e0f7e23534a3debdc90b3297
Knox S. Long <long@stsci.edu>
2024-12-13 12:07:20 -0600

    Clean up of SumCFrame

--- SumCframe.py
+++ SumCframe.py
@@ -12,9 +12,20 @@ number of files
 
 Command line usage (if any):
 
-    usage: SummarizeCFrame exp_start expstop delta
+    usage: SumCFrame [-h] [-emin 900] [-out whatever] [-ver 1.1.3] [-drp_all drp_file] exp_start exp_stop [delta]
 
 Description:  
+
+    where -h prints out this help and quits
+            =emin 900           resets the minimum exposure to another value
+            -out whatever       sets the root for the output fits file to soemthing other than the default
+            -ver 1.1.3          resets the location to lookd for the drp_all file
+            -drp_all drp_file   causes the program to read a speccial drp_all file or table named 'drp_file'
+                                as opposed to the drp_all file associated with the version of processing
+            exp_start           The starting exposure to consider
+            exp_stop            The exposure snumber to stop on
+            [delta]             The number of 'good' exposures to skip
+
 
 Primary routines:
 
@@ -39,8 +50,26 @@ from datetime import datetime
 from astropy.wcs import WCS
 
 
-def read_drpall(drp_ver='1.1.0'):
-    DRPFILE='drpall-%s.fits' % (drp_ver)
+def read_drpall(filename='',drp_ver='1.1.0'):
+    '''
+    This reads a drp_all fits file or
+    an ascii table that contains the table
+    and returns the table
+    '''
+
+    if filename.count('txt') or filename.count('.tab'):
+        try:
+            drp_tab=ascii.read(filename)
+            return drp_tab
+        else:
+            print('Error: Could not locate : ', filename)
+            return []
+    
+    elif filename!='':
+        DRPFILE='drpall-%s.fits' % (drp_ver)
+    else:
+        DRPFILE=filename
+
     # First try to locate the DRP file locally, otherwise
     if os.path.isfile(DRPFILE):
         xfile=DRPFILE
@@ -124,108 +153,6 @@ def scifib(xtab,select='all',telescope=''):
     return ztab
 
 
-def get_med_spec(filename= '/Users/long/Projects/lvm_data/sas/sdsswork/lvm/spectro/redux/1.1.0/0011XX/11111/60192/lvmSFrame-00004336.fits'):
-
-    if filename.count('SFrame'):
-        filename=filename.replace('SFrame','CFrame')
-
-    try:
-        x=fits.open(filename)
-    except:
-        print('gets_spec: Could not open %s' % filename)
-        return
-
-    xtab=Table(x['SLITMAP'].data)
-
-    science_fibers=scifib(xtab,select='science',telescope='Sci')
-    # skye_fibers=scifib(xtab,select='SKY',telescope='SkyE')
-    # skyw_fibers=scifib(xtab,select='SKY',telescope='SkyW')
-
-    wav=x['WAVE'].data
-    sci_flux=x['FLUX'].data[science_fibers['fiberid']-1]
-    sky_e_flux=x['SKY_EAST'].data[science_fibers['fiberid']-1]
-    sky_w_flux=x['SKY_WEST'].data[science_fibers['fiberid']-1]
-    sci_mask=x['MASK'].data[science_fibers['fiberid']-1]
-    sci_flux=np.ma.masked_array(sci_flux,sci_mask)
-    sky_e_flux=np.ma.masked_array(sky_e_flux,sci_mask)
-    sky_w_flux=np.ma.masked_array(sky_w_flux,sci_mask)
-
-    sci_flux_med=np.ma.median(sci_flux,axis=0)
-    sky_e_flux_med=np.ma.median(sky_e_flux,axis=0)
-    sky_w_flux_med=np.ma.median(sky_w_flux,axis=0)
-
-    # print(sci_flux_med.shape,sky_e_flux_med.shape,sky_w_flux_med.shape)
-
-
-    return wav, sci_flux_med, sky_e_flux_med,sky_w_flux_med
-
-
-def make_med_spec(xtab,data_dir,outfile=''):
-    i=0
-    select=[]
-    xfiles=[]
-    while i < len(xtab):
-        xfile='%s/%s' % (data_dir,xtab['location'][i])
-        if xfile.count('SFrame'):
-            xfile=xfile.replace('SFrame','CFrame')
-        # print(xfile)
-        if os.path.isfile(xfile):
-            select.append(i)
-            xfiles.append(xfile)
-        i+=1
-    print('There are %d files to process' % (len(select)))
-    xtab=xtab[select]
-    # print(xfiles)
-    i=0
-    xsci_flux=[]
-    xsci_sky_e=[]
-    xsci_sky_w=[]
-    while i<len(xfiles):
-        wav, sci_flux, sky_e_flux,sky_w_flux=get_med_spec(xfiles[i])
-        xsci_flux.append(sci_flux)
-        xsci_sky_e.append(sky_e_flux)
-        xsci_sky_w.append(sky_w_flux)
-        if i%10==0:
-            print('Finished %d of %d' % (i,len(xfiles)))
-                                          
-        i+=1
-    wav=np.array(wav)
-    xsci_flux=np.array(xsci_flux)
-    xsci_sky_e=np.array(xsci_sky_e)
-    xsci_sky_w=np.array(xsci_sky_w)
-    print(xsci_flux.shape,xsci_sky_e.shape,xsci_sky_w.shape)
-    hdu1 = fits.PrimaryHDU(data=None)
-    hdu1.header['Title'] = 'CFrame_Summmary'
-    hdu2= fits.ImageHDU(data=wav,name='WAVE')
-    hdu3=fits.ImageHDU(data=xsci_flux,name='FLUX')
-    hdu4=fits.ImageHDU(data=xsci_sky_e,name='SKY_EAST')
-    hdu5=fits.ImageHDU(data=xsci_sky_w,name='SKY_WEST')
-    hdu6 = fits.BinTableHDU(xtab, name='drp_all')
-
-    wmin=wav[0]
-    dwave=0.5
-
-    wcs = WCS(naxis=2)
-    wcs.wcs.crpix = [1, 1]  # Reference pixel (1-based index)
-    wcs.wcs.crval = [wmin, 0]  # Coordinates at the reference pixel: minimum wavelength and line number 0
-    wcs.wcs.cdelt = [dwave, 1]  # Pixel scale: 0.5 Angstroms per pixel in wavelength, 1 per pixel in line number
-    wcs.wcs.ctype = ['WAVE', 'LINE']  # Coordinate types: wavelength and line number
-
-    hdu3.header.update(wcs.to_header())
-    hdu4.header.update(wcs.to_header())
-    hdu5.header.update(wcs.to_header())
-
-    hdul = fits.HDUList([hdu1, hdu2, hdu3,hdu4,hdu5,hdu6])
-
-    if outfile=='':
-        outfile='XCFrame_test.fits'
-    else:
-        if outfile.count('.fits')==0:
-            outfile=outfile+'.fits'
-    hdul.writeto(outfile,overwrite=True)
-    print('Wrote results to %s' % outfile)
-    return
-
 def sum_frames(xfiles):
     '''
     Given a bunch of rss files containing a FLUX extension, this
@@ -250,12 +177,21 @@ def sum_frames(xfiles):
     print('Finished xsum',xsum.shape)
     return xsum
 
-def doit(exp_start=4000,exp_stop=8000,delta=5,exp_min=900.,out_name='',drp_ver='1.1.0'):
+def files_select(filename='',exp_start=4000,exp_stop=8000,delta=5,exp_min=900.):
+    '''
+    Read the drp_all table and select files to process from this table
+    '''
     data_dir=find_top()
-    xtab=read_drpall(drp_ver)
+    xtab=read_drpall(filename,drp_ver)
     print('got drp all')
     xtab=xselect(xtab,exp_start,exp_stop,delta)
-    # At this point we have the portion of the drp all file that contains the exposures we want to sum
+    return xtab
+
+
+def process_files(ztab):
+    '''
+    Having decided what needs processing do the work
+    '''
     i=0
     select=[]
     xfiles=[]
@@ -296,16 +232,29 @@ def doit(exp_start=4000,exp_stop=8000,delta=5,exp_min=900.,out_name='',drp_ver='
     return
 
 
+def doit(filename='',exp_start=4000,exp_stop=8000,delta=5,exp_min=900.,out_name='',drp_ver='1.1.0'):
+    '''
+    The main routine allowing one to process the data
+    '''
+    xtab=files_select(filename,exp_start,exp_stop,delta,exp_min)
+
+    if len(xtab)>0:
+        process_files(xtab)
+    return
+
+
 def steer(argv):
     '''
-    SummarizeCFrame exp_start expstop delta
+    SumCFrame -h -emin 1000 -[-out whatever] [-drp_all drp_file] exp_start expstop [delta]
     '''
     exp_start=-1
     exp_stop=-1
     delta=-1
 
     exp_min=900
+    input_file=''
     out_name=''
+    xver='1.1.0'
 
     i=1
     while i<len(argv):
@@ -318,6 +267,12 @@ def steer(argv):
         elif argv[i]=='-out':
             i+=1
             out_name=(argv[i])
+        elif argv[i]=='-drp_all':
+            i+=1
+            input_file=argv[i]
+        elif argv[i]=='-ver':
+            i+=1
+            xver=argv[i]
         elif argv[i][0]=='-':
             print('Unknown option : ',argv)
         elif exp_start<0:
@@ -332,7 +287,7 @@ def steer(argv):
         delta=1
                 
 
-    doit(exp_start,exp_stop,delta,exp_min,out_name,drp_ver='1.1.0')
+    doit(input_file,exp_start,exp_stop,delta,exp_min,out_name,xver)
 
 
 
